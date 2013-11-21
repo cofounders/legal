27ff953ec22289a7f260cdd790bb95a2f38d86c5
Sebastiaan Deckers <seb@ninja.sg>
2013-11-21 18:50:22 +0800

    Poor man's rich text editor

---
+++ scripts/editor.js
@@ -0,0 +1,15 @@
+/** Rich text editor
+ **/
+
+(function (window, undefined) {
+
+	document.getElementById('document')
+		.setAttribute('contenteditable', true);
+
+	// var nodes = document.querySelectorAll('p,li,dd,dt,h1,h2,h3,h4,h5,h6');
+	// var paragraphs = Array.prototype.slice.call(nodes);
+	// paragraphs.forEach(function (el) {
+	// 	el.setAttribute('contenteditable', true);
+	// });
+
+})(this);
