2f381f5ddd25edc982d3d2bdcf3169f1ca7af43d
Sebastiaan Deckers <seb@ninja.sg>
2013-11-21 18:53:13 +0800

    Constraining editor to just the legal template

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,15 +1,9 @@
-/** Rich text editor
+/** Poor man's rich text editor
  **/
 
 (function (window, undefined) {
 
-	document.getElementById('document')
+	document.querySelector('#document > article')
 		.setAttribute('contenteditable', true);
-
-	// var nodes = document.querySelectorAll('p,li,dd,dt,h1,h2,h3,h4,h5,h6');
-	// var paragraphs = Array.prototype.slice.call(nodes);
-	// paragraphs.forEach(function (el) {
-	// 	el.setAttribute('contenteditable', true);
-	// });
 
 })(this);
